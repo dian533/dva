cbf48a5c7fbae6c6ba3f80e4bec07ec67df36135
shanshan.hongss <shanshan.hongss@alibaba-inc.com>
2017-10-23 17:05:54 +0800

    兼容models异步加载后被default包裹的情况

--- packages/dva/src/dynamic.js
+++ packages/dva/src/dynamic.js
@@ -2,7 +2,8 @@ import { asyncComponent } from 'react-async-component';
 
 const cached = {};
 function registerModel(app, model) {
-  if (!cached[model.namespace]) {
+  const ns = model.namespace || model.default.namespace;
+  if (!cached[ns]) {
     app.model(model);
     cached[model.namespace] = 1;
   }
@@ -11,23 +12,26 @@ function registerModel(app, model) {
 export default function dynamic(config) {
   const { app, models: resolveModels, component: resolveComponent } = config;
   return asyncComponent({
-    resolve: config.resolve || function () {
-      const models = typeof resolveModels === 'function' ? resolveModels() : [];
-      const component = resolveComponent();
-      return new Promise((resolve) => {
-        Promise.all([...models, component]).then((ret) => {
-          if (!models || !models.length) {
-            return resolve(ret[0]);
-          } else {
-            const len = models.length;
-            ret.slice(0, len).forEach((m) => {
-              registerModel(app, m);
-            });
-            resolve(ret[len]);
-          }
+    resolve:
+      config.resolve ||
+      function() {
+        const models =
+          typeof resolveModels === 'function' ? resolveModels() : [];
+        const component = resolveComponent();
+        return new Promise(resolve => {
+          Promise.all([...models, component]).then(ret => {
+            if (!models || !models.length) {
+              return resolve(ret[0]);
+            } else {
+              const len = models.length;
+              ret.slice(0, len).forEach(m => {
+                registerModel(app, m);
+              });
+              resolve(ret[len]);
+            }
+          });
         });
-      });
-    },
+      },
     ...config,
   });
 }
